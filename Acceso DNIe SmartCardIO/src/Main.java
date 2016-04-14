1e567809c7dc72dd056c8db319f1e01258a7b20d
Jose Luis Cabeza Diaz <jlcd0004@red.ujaen.es>
2016-04-15 00:52:37 +0200

    Autenticación en servidor completa.
    Mediante hash y mediante autenticación simple.

--- Acceso DNIe SmartCardIO/src/Main.java
+++ Acceso DNIe SmartCardIO/src/Main.java
@@ -1,9 +1,5 @@
 
 import java.util.Scanner;
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -20,36 +16,66 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         ObtenerDatos od = new ObtenerDatos();
         Autenticacion aut = new Autenticacion();
-        String user, nif, nomAp, key, datos, hash, datosEn, user64, nif64, hash64;
+        String user, nif, nomAp, key, datos, hash, datosEn, user64, nif64, hash64, key64;
+        int tipo = 0;
 
         nif = od.LeerNIF();
         nomAp = od.LeerNombreApell();
         user = od.usuario(nomAp);
 
         System.out.println("NIF: " + nif);
-        System.out.println("Nombre: " + nomAp);
+        System.out.println("Nombre y apellidos: " + nomAp);
         System.out.println("Usuario: " + user);
-
-        System.out.println("\nIntroduzca su clave: ");
-        key = scan.nextLine();
-
-        datos = user + nif + key;
-        System.out.println("Datos para hash= " + datos);
-
-        hash = od.hashSha1(datos);
-        System.out.println("Hash= " + hash);
-
-        user64 = od.base64(user);
-        nif64 = od.base64(nif);
-        hash64 = od.base64(hash);
-
-        datosEn = "user=" + user64 + "&dni=" + nif64 + "&hash=" + hash64;
-
-        System.out.println("Datos para envio= " + datosEn);
         
-        System.out.println(aut.result(datosEn));
-        
-        
+        while (tipo != 3) {
+            System.out.print("\nSeleccione tipo de autenticado: "
+                    + "\n1-Simple"
+                    + "\n2-Hash"
+                    + "\n3-Salir"
+                    + "\n\nOpcion: ");
+            tipo = scan.nextInt();
+            scan.nextLine();
+
+            switch (tipo) {
+                case 1:
+                    System.out.print("\nIntroduzca su clave: ");
+                    key = scan.nextLine();
+
+                    user64 = od.base64(user);
+                    nif64 = od.base64(nif);
+                    key64 = od.base64(key);
+
+                    datosEn = "user=" + user64 + "&dni=" + nif64 + "&password=" + key64;
+
+                    System.out.println("\nDatos para envio= " + datosEn);
+
+                    System.out.println(aut.result(datosEn));
+                    break;
+
+                case 2:
+                    System.out.print("\nIntroduzca su clave: ");
+                    key = scan.nextLine();
+
+                    datos = user + nif + key;
+                    System.out.println("\nDatos para hash= " + datos);
+
+                    hash = od.hashSha1(datos);
+                    System.out.println("Hash= " + hash);
+
+                    user64 = od.base64(user);
+                    nif64 = od.base64(nif);
+                    hash64 = od.base64(hash);
+
+                    datosEn = "user=" + user64 + "&dni=" + nif64 + "&hash=" + hash64;
+
+                    System.out.println("Datos para envio= " + datosEn);
+
+                    System.out.println("\nRESULTADO --> " + aut.result(datosEn));
+                    break;
+
+            }
+        }
+
+        System.out.println("\n¡Adios!");
     }
-
 }
--- dnie/autentica.php
+++ dnie/autentica.php
@@ -9,18 +9,18 @@
 
     <body>
         <div id='banner'>
-            
-              <h2>Pr&aacute;ctica 3. Implementación de un servicio b&aacute;sico de autenticaci&oacute;n con DNIe</h2>
+
+            <h2>Pr&aacute;ctica 3. Implementación de un servicio b&aacute;sico de autenticaci&oacute;n con DNIe</h2>
             <h3>Autenticación con datos públicos del DNIe</h3>
 
         </div>
         <div id='main'>
             <?php
-            $user=NULL;
-            $dni=NULL;
-            $password=NULL;
-            $autenticado=false;
-            
+            $user = NULL;
+            $dni = NULL;
+            $password = NULL;
+            $autenticado = false;
+
             $user = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);
 
 
@@ -28,63 +28,74 @@
 
                 $dni = filter_input(INPUT_POST, 'dni', FILTER_SANITIZE_STRING);
                 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-                
-                if($dni==NULL)
-                ?>
+
+                if ($dni == NULL)
+                    
+                    ?>
                 <h3>Autenticando mediante POST</h3>
                 <?php
-                
             } else {
                 $user = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_STRING);
                 if ($user != NULL) {
+                    $user = base64_decode($user);
                     $dni = filter_input(INPUT_GET, 'dni', FILTER_SANITIZE_STRING);
+                    $dni = base64_decode($dni);
                     $password = filter_input(INPUT_GET, 'password', FILTER_SANITIZE_STRING);
+                    $password = base64_decode($password);
+                    $hash = filter_input(INPUT_GET, 'hash', FILTER_SANITIZE_STRING);
+                    $hash = base64_decode($hash);
                     ?>
                     <h3>Autenticando mediante GET</h3>
                     <?php
-                   
                 } else {
                     die('No se ha enviado el usuario');
                 }
             }
-            
-             $link = mysql_connect('localhost:3306', 'root', '12345') or die('No se puede conectar con el servidor');
-                if (!$link) {
-                    die('Could not connect to MySQL: ' . mysql_error());
-                }
+            $link = mysql_connect('localhost:3306', 'root', '123456#') or die('No se puede conectar con el servidor');
+            if (!$link) {
+                die('Could not connect to MySQL: ' . mysql_error());
+            }
+            mysql_select_db('dniauth', $link) or die('No se puede conectar con la base de datos');
 
-                mysql_select_db('dniauth', $link) or die('No se puede conectar con la base de datos');
+            if ($password != NULL) {
 
                 $sql = "SELECT * FROM users";
                 $resultado = mysql_query($sql);
                 while ($row = mysql_fetch_assoc($resultado)) {
 
-                    if($row["user"]==$user && $row["dni"]==$dni && $row["password"]==$password)// . " " . $row["dni"] . "</li>";
-                    {
-                        
-                        $autenticado=true;
+                    if ($row["user"] == $user && $row["dni"] == $dni && $row["password"] == $password) {// . " " . $row["dni"] . "</li>";
+                        $autenticado = true;
                     }
                 }
-                
-                if($autenticado)
-                {
-                     ?>
-                    <h4>Bienvenido <strong><?php echo $user . "</strong> con DNI " . $dni;?></h4>
-                    <?php
-                }else
-                    {
-                     ?>
-                    <h4>Error de autenticaci&oacute;n</h4>
-                    <?php echo "<p>No se encuentra a <strong>".$user . "</strong> o los datos de DNI y clave son incorrectos</p>";?>
-                    <?php
-                    }
+            } else {
+
+                $sql = "SELECT * FROM users WHERE dni='" . $dni . "'";
+                $resultado = mysql_query($sql);
+                $row = mysql_fetch_assoc($resultado);
+                $datosHash = $user . $dni . $row["password"];
+                $hash2 = sha1($datosHash);
+                if ($hash2 == $hash) {
+                    $autenticado = true;
+                }
+            }
+
+            if ($autenticado) {
+                ?>
+                <h4>Bienvenido <strong><?php echo $user . "</strong> con DNI " . $dni; ?></h4>
+                <?php
+            } else {
+                ?>
+                <h4>Error de autenticaci&oacute;n</h4>
+                <?php echo "<p>No se encuentra a <strong>" . $user . "</strong> o los datos de DNI y clave son incorrectos</p>"; ?>
+                <?php
+            }
             ?>
-                   
-                       
+
+
         </div>
         <div id="foot">
-             <a href="index.php">Volver</a>
-               <h2>Aplicaciones Telemáticas para la Administración</h2>
+            <a href="index.php">Volver</a>
+            <h2>Aplicaciones Telemáticas para la Administración</h2>
             <p>Grado en Ingenier&iacute;a Telem&aacute;tica y Grado en Ingenier&iacute;a de Tecnolog&iacute;as de Telecomunicaci&oacute;n</p>
             <p>DEPARTAMENTO DE INGENIERÍA DE TELECOMUNICACIÓN</p>
 
